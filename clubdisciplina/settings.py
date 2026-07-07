599b4f7f5c1a49c949fe12d718b5ea826ac9cfe6
fjmme28-eng <fjmme28@gmail.com>
2026-07-07 11:40:14 -0600

    Eliminar debug de base de datos

--- clubdisciplina/settings.py
+++ clubdisciplina/settings.py
@@ -95,9 +95,6 @@ DATABASES = {
     )
 }
 
-print("BASE DE DATOS:", DATABASES["default"])
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
